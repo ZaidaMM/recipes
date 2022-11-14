eb0a5a3e644054c4f562dbe38a92d32144e34bbd
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-11-14 10:38:45 +0000

    about page set up

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,35 @@
 import React from "react"
 import Layout from "../components/Layout"
+import { StaticImage } from "gatsby-plugin-image"
+import { Link } from "gatsby"
 
 const about = ({ children }) => {
   return (
     <Layout>
-      <h1>ABOUT</h1>
+      <main className="page">
+        <section className="about-page">
+          <article>
+            <h2>Lorem ipsum dolor sit amet</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit
+              amet tellus in leo eleifend imperdiet.
+            </p>
+            <p>
+              Vestibulum pellentesque leo quam, in commodo lectus bibendum eget.
+              Suspendisse mattis lectus nec elit venenatis hendrerit.
+            </p>
+            <Link to="/contact" className="btn">
+              contact
+            </Link>
+          </article>
+          <StaticImage
+            src="../assets/images/about.jpeg"
+            alt="Person puring salt in bowl"
+            className="about-img"
+            placeholder="blurred"
+          />
+        </section>
+      </main>
     </Layout>
   )
 }
