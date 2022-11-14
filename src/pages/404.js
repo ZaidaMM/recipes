149b290a07e89314a77d7a4365e57984ac1a122f
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-11-14 05:15:34 +0000

    error page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,6 +2,15 @@ import React from "react"
 import Layout from "../components/Layout"
 
 const error = () => {
-  return <Layout>Error Page</Layout>
+  return (
+    <Layout>
+      <div className="error-page">
+        <section>
+          <h1>404</h1>
+          <h3>page not found</h3>
+        </section>
+      </div>
+    </Layout>
+  )
 }
 export default error
