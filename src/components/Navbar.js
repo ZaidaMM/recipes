03d0ff13b7882387e0eb386fe035fdd17e7fbb42
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-11-14 06:25:30 +0000

    navbar setup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,43 @@
 import React from "react"
 import { Link } from "gatsby"
+import { FiAlignJustify } from "react-icons/fi"
 
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">home</Link>
-        </li>
-        <li>
-          <Link to="recipes">recipes</Link>
-        </li>
-        <li>
-          <Link to="tags">tags</Link>
-        </li>
-        <li>
-          <Link to="/about">about</Link>
-        </li>
-        <li>
-          <Link to="contact">contact</Link>
-        </li>
-      </ul>
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <Link to="/" className="logo">
+            <span>Your</span>Recipes
+          </Link>
+          <button className="nav-btn">
+            <FiAlignJustify />
+          </button>
+        </div>
+        <div className="nav-links show-links">
+          <Link to="/" className="nav-link " activeClassName="active-link">
+            home
+          </Link>
+          <Link
+            to="/recipes"
+            className="nav-link "
+            activeClassName="active-link"
+          >
+            recipes
+          </Link>
+          <Link to="/tags" className="nav-link " activeClassName="active-link">
+            tags
+          </Link>
+          <Link to="/about" className="nav-link " activeClassName="active-link">
+            about
+          </Link>
+        </div>
+        <div className="nav-link contact-link">
+          <Link to="/contact" className="btn">
+            contact
+          </Link>
+        </div>
+      </div>
     </nav>
   )
 }
